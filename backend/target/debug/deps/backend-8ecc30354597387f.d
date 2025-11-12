26d5382c0780ff4e0754d2516465f65a9f9157b1
Aman Kumar <amansinha327@gmail.com>
2025-11-12 13:07:15 +0000

    Initial Rust backend setup

---
+++ backend/target/debug/deps/backend-8ecc30354597387f.d
@@ -0,0 +1,5 @@
+/home/siphonite/project1/reddit_ideas/backend/backend/target/debug/deps/backend-8ecc30354597387f.d: src/main.rs
+
+/home/siphonite/project1/reddit_ideas/backend/backend/target/debug/deps/backend-8ecc30354597387f: src/main.rs
+
+src/main.rs:
